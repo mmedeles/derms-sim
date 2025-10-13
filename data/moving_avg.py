051373d1c0730dd4fbdfb0f8e3d784196813ad43
divide <divide654@gmail.com>
2025-10-13 16:40:04 -0500

    Update moving_avg.py

--- data/moving_avg.py
+++ data/moving_avg.py
@@ -6,8 +6,11 @@ class Node_MA:
         self.last_n = []
     def average(self):
         return sum(self.last_n) / len(self.last_n)
+    def std(self):
+        return np.std(self.last_n,ddof=1)    
     def update(self,new_value):
         self.last_n.append(new_value)
         if len(self.last_n) > self.n:
             self.last_n.pop(0)
         return self.average()
+    
